14c9a802d07a5808b27899a40b874ebfa97db811
julien44830 <deniaudjulien0@gmail.com>
2024-10-10 10:50:12 +0200

    ajout du bouton like front

--- client/src/components/CardRepos.tsx
+++ client/src/components/CardRepos.tsx
@@ -1,36 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import type { Repo } from "../@type/interface";
 
 interface CardReposProps {
     repo: Repo;
 }
-// name={repo.name}
-// url={repo.url}
-// id={repo.id}
-// isFavorite={repo.isFavorite}
 
-const CardRepos: React.FC<CardReposProps> = ({ repo }) => {
+const CardRepos: React.FC<CardReposProps> = ({
+    repo,
+}) => {
+    const [isFavorite, setIsfavorite] = useState<boolean>(false);
+
     return (
-        <Link to={`/detail/${repo.id}`} className ="card">
+        <section
+            
+            className="card"
+        >
             <p>{repo.name}</p>
             <section>
                 <p>langage utilisé :</p>
                 <ul>
                     <li>bientôt disponible</li>
                 </ul>
-                <a
-                    className={`section-card-lien ${
-                        repo.status && repo.status.label === "Privé"
-                            ? "desableLink"
-                            : ""
-                    }`}
-                    href={repo.url}
-                    target="_blank"
-                >
-                    Accédez au repo
-                </a>
+                <section className="link-img-like">
+                    <Link
+                        className={`section-card-lien ${
+                            repo.status && repo.status.label === "Privé"
+                                ? "desableLink"
+                                : ""
+                        }`}
+                        to={`/detail/${repo.id}`}
+                    >
+                        détail du repo
+                    </Link>
+                    <img onClick={()=>setIsfavorite(!isFavorite)} src={isFavorite ? "/heart_like.png":"/heart_dislike.png"} alt="" className="favorite" />
+                </section>
             </section>
-        </Link>
+        </section>
     );
 };
 
