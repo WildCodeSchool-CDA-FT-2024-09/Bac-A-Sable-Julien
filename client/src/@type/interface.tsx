fce06092850f8ca3b287634b8d1abb33d7633c11
julien44830 <deniaudjulien0@gmail.com>
2024-10-15 11:42:29 +0200

    install codegen creation du shema pour les query dans home page

--- client/src/@type/interface.tsx
+++ client/src/@type/interface.tsx
@@ -1,21 +1,22 @@
 export interface Language {
-  id: number;
-  label: string;
+    id?: number;
+    label: string | undefined;
 }
 
 export interface Repo {
-  id: number;
-  name: string;
-  languages: Language[];
-  url: string;
-  status: {
-      label: string;
-  };
+    idGit: string;
+    id: number;
+    name: string;
+    languages?: Language[] | null; // Accepter undefined ou null
+    url: string;
+    status: {
+        label: string;
+    };
 }
 
 export interface CardReposProps {
-  repos: Repo[];
-  setCommentOpen: (open: boolean) => void;
-  commentOpen: boolean;
-  statusComment: () => void;
+    repos: Repo[];
+    setCommentOpen: (open: boolean) => void;
+    commentOpen: boolean;
+    statusComment: () => void;
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,88 +1,71 @@
 import { AnimatedBackground } from "animated-backgrounds";
-import { useQuery, gql } from "@apollo/client";
-import { Repo } from "../@type/interface";
+// import { Repo } from "../@type/interface";
 import SelectLang from "../components/SelectLang";
 import CardRepos from "../components/CardRepos";
 import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const GET_REPOS = gql`
-    query GetAllReposFilter($filter: String) {
-        getAllReposFilter(filter: $filter) {
-            id
-            idGit
-            isFavorite
-            name
-            url
-            status {
-                label
-            }
-            languages {
-                label
-            }
-        }
-        GetAllLang {
-            id
-            label
-        }
-    }
-`;
+import {
+    useGetAllReposFilterQuery,
+    GetAllReposFilterQuery,
+} from "../generated/graphql-type";
 
 export default function HomePage() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [selectLang, setSelectLang] = useState<string | undefined>("");
 
-        // Mettre à jour l'URL lorsque la langue sélectionnée change
-        useEffect(() => {
-            if (selectLang) {
-                setSearchParams({ filter: selectLang });
-            }
-        }, [selectLang, setSearchParams]);
-    
-        // Lire la langue depuis les paramètres de l'URL au chargement
-        useEffect(() => {
-            const filter = searchParams.get("filter");
-            if (filter) {
-                setSelectLang(filter);
-            }
-        }, [searchParams]);
+    // Mettre à jour l'URL lorsque la langue sélectionnée change
+    useEffect(() => {
+        if (selectLang) {
+            setSearchParams({ filter: selectLang });
+        }
+    }, [selectLang, setSearchParams]);
 
-    const { loading, error, data, refetch } = useQuery(GET_REPOS, {
+    // Lire la langue depuis les paramètres de l'URL au chargement
+    useEffect(() => {
+        const filter = searchParams.get("filter");
+        if (filter) {
+            setSelectLang(filter);
+        }
+    }, [searchParams]);
+
+    const { loading, error, data, refetch } = useGetAllReposFilterQuery({
         variables: { filter: selectLang },
     });
 
     if (loading) return <h1>loading ...</h1>;
     if (error) return <p>Erreur: {error.message}</p>;
-
-    return (
-        <main>
-            <AnimatedBackground animationName="rainbowWaves" />
-            <h1>Les projets de Julien</h1>
-            <SelectLang
-                lang={data.GetAllLang}
-                setSelectLang={setSelectLang}
-            />
-            <button
-                onClick={() => {
-                    refetch()
-                        .then(({ data }) => {
-                            console.log("%c⧭", "color: #ffcc00", data);
-                        })
-                        .catch((error) => {
-                            console.error(error);
-                        });
-                }}
-            >
-                Recharger les dépôts
-            </button>{" "}
-            <section className="section-card-repos">
-                {data.getAllReposFilter.map((repo: Repo) => (
-                    <CardRepos
-                        key={repo.id}
-                        repo={repo}
-                    />
-                ))}
-            </section>
-        </main>
-    );
+    if (data) {
+        return (
+            <main>
+                <AnimatedBackground animationName="rainbowWaves" />
+                <h1>Les projets de Julien</h1>
+                <SelectLang
+                    lang={data?.GetAllLang || []}
+                    setSelectLang={setSelectLang}
+                />
+                <button
+                    onClick={() => {
+                        refetch()
+                            .then(({ data }) => {
+                                console.log("%c⧭", "color: #ffcc00", data);
+                            })
+                            .catch((error) => {
+                                console.error(error);
+                            });
+                    }}
+                >
+                    Recharger les dépôts
+                </button>{" "}
+                <section className="section-card-repos">
+                    {data.getAllReposFilter.map((repo) => (
+                        <CardRepos
+                            key={repo.id}
+                            repo={repo}
+                        />
+                    ))}
+                </section>
+            </main>
+        );
+    }
 }
