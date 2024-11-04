abb2199cd88cdf484398721c5c15f4e7ab366edb
julien44830 <deniaudjulien0@gmail.com>
2024-11-04 12:19:47 +0100

    add address appolo serv

--- client/src/service/connexion.ts
+++ client/src/service/connexion.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
 
-  uri: "http://localhost:4000/",
+  uri: "https://bac-a-sable-julien-1.onrender.com",
   cache: new InMemoryCache()
 }
 )
