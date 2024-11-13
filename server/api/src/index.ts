e8b23cdd542f740c5750f73a51046c2a3d047fb1
julien44830 <deniaudjulien0@gmail.com>
2024-11-13 14:17:08 +0100

    change data-source for deploy

--- server/api/src/index.ts
+++ server/api/src/index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+dotenv.config();
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { buildSchema } from "type-graphql";
@@ -11,21 +12,28 @@ import LanguageResolver from "./resolver/lang.resolver";
 const port = parseInt(process.env.PORT || "4000", 10);
 console.log('%c⧭', 'color: #00e600', port);
 
-dotenv.config();
 
 
 (async () => {
-  await dataSource.initialize();
-  const schema = await buildSchema({
-    resolvers: [RepoResolver, LanguageResolver],
-  });
+  try {
+    await dataSource.initialize();
+    console.log("Data source initialized successfully");
 
-  const server = new ApolloServer({
-    schema,
-  });
+    const schema = await buildSchema({
+      resolvers: [RepoResolver, LanguageResolver],
+    });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port },
-  });
-  console.log(`🚀  Server ready at: ${url}`);
+    const server = new ApolloServer({
+      schema,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+      listen: { port },
+    });
+
+    console.log(`🚀  Server ready at: ${url}`);
+  } catch (error) {
+    console.error("Error during server initialization: ", error);
+  }
 })();
+
